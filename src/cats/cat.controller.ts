ba604910b0d728269e7566155ee69d831178691f
Logan <lrlivingston@live.com>
2018-08-05 11:37:05 -0500

    remove aws from controller

--- src/cats/cat.controller.ts
+++ src/cats/cat.controller.ts
@@ -1,13 +1,5 @@
 import {Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Req, Res} from '@nestjs/common';
 import {CatService} from './cat.service';
-import * as AWS from 'aws-sdk';
-
-const AWS_S3_BUCKET_NAME = 'catpartycatphotos';
-const s3 = new AWS.S3();
-AWS.config.update({
-    accessKeyId: 'AKIAJRKDOVLQEEGXZYRA',
-    secretAccessKey: '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',
-});
 
 @Controller('cat')
 export class CatController {
